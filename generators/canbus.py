5a503f0a50a1c37615100789a82458fb70ae56e8
Seydi Regner <seydi.regner@outlook.com>
2022-03-29 15:37:38 +0200

    completed canbus.py

--- generators/canbus.py
+++ generators/canbus.py
@@ -73,7 +73,7 @@ bool canbus_is_{name}_valid(void)
     return text
 
 
-def set_min_function(prototype_set, prototype_get, valid, if_float_multiply, index, start, length):
+def set_min_max_function(prototype_set, valid, if_float_multiply, index, start, length):
 
     text = \
         f"""
@@ -94,57 +94,28 @@ def set_min_function(prototype_set, prototype_get, valid, if_float_multiply, ind
               """
     return text
 
+def convert_function():
+    text = """ 
 
-def set_only_min_or_max_function(prototype_set, valid, if_float_multiply, index, start, length):
+static int64_t convert(uint64_t value, uint8_t length)
+{
+    if (value & (1ULL << (length - 1)))
+    {
+        value |= (~0ULL << length);
+    }
 
-    text = \
-        f"""
-{prototype_set}
-{{
-    bool status = false;
-
-    if ({valid})
-    {{\
-        {if_float_multiply}
-        can_signal_write({index}, {start}, {length}, (uint64_t)value);
-        can_signal_write({index}, {start + length}, 1, 1);
-        status = true;
-    }}
-
-    return status;
-}}
-              """
+    return (int64_t)value;
+}"""
     return text
-
-
-def set_max_function(prototype_set, prototype_get, valid, if_float_multiply, index, start, length):
-    text = \
-        f"""
-{prototype_set}
-{{
-    bool status = false;
-
-    if ({valid})
-    {{\
-        {if_float_multiply}
-        can_signal_write({index}, {start}, {length}, (uint64_t)value);
-        can_signal_write({index}, {start + length}, 1, 1);
-        status = true;
-    }}
-
-    return status;
-}}
-              """
-    return text
-
 
 def get_canbus_source(node, mode, messages):
     get = ''
     set = ''
     control_bit = ''
+    convert = ''
 
     for index, message in enumerate(messages):
-        for signal_index, signal in enumerate(message['signals']):
+        for signal in message['signals']:
 
             name = signal.get('name')
             type = signal.get('type')
@@ -173,7 +144,8 @@ def get_canbus_source(node, mode, messages):
 
                     # if negative value in range use convert function
                     if signal['range'][0] < 0:
-                        return_text = f"return ({type})convert_if_negative(can_signal_read({index}, {start}, {length}), {length}){if_float_devide};"
+                        return_text = f"return ({type})convert(can_signal_read({index}, {start}, {length}), {length}){if_float_devide};"
+                        convert = convert_function()
                     else:
                         return_text = f"return ({type})can_signal_read({index}, {start}, {length}){if_float_devide};"
                 else:
@@ -185,63 +157,23 @@ def get_canbus_source(node, mode, messages):
                     length -= 1
 
                 # setters and getters function names
-                if message['setter'] == node and node in signal['getters']:
+                if message['setter'] == node:
                     prototype_set = f"bool canbus_set_{name}({type} value)"
+                    if node not in signal['getters']:
+                        if mode == 'dev':
+                            prototype_get = f"{type} canbus_test_get_{name}(void)"
+                if node in signal['getters']:
                     prototype_get = f"{type} canbus_get_{name}(void)"
-
-                elif message['setter'] == node and node not in signal['getters']:
-                    prototype_set = f"bool canbus_set_{name}({type} value)"
-                    prototype_get = ''
-                    if mode == 'dev':
-                        prototype_get = f"{type} canbus_test_get_{name}(void)"
-
-                elif message['setter'] != node and node in signal['getters']:
-                    prototype_set = ''
-                    prototype_get = f"{type} canbus_get_{name}(void)"
-                    if mode == 'dev':
-                        prototype_set = f"bool canbus_test_set_{name}({type} value)"
+                    if message['setter'] != node:
+                        if mode == 'dev':
+                            prototype_set = f"bool canbus_test_set_{name}({type} value)"      
 
                 # generate setters functions
                 if prototype_set != '':
                     if 'calibration' in signal:
-
-                        previous_name = message['signals'][signal_index - 1]['name']
-                        next_name = ''
-                        if message['signals'][-1]['name'] != name:
-                            next_name = message['signals'][signal_index + 1]['name']
-
-                        if name.find('min') != -1:
-                            if previous_name.find('max') != -1:
-                                previous_name = previous_name.replace(
-                                    previous_name[len(previous_name) - 3:], 'min')
-                            if next_name.find('max') != -1:
-                                next_name = next_name.replace(
-                                    next_name[len(next_name) - 3:], 'min')
-
-                            if name == previous_name or name == next_name:
-                                set += set_min_function(
-                                    prototype_set, prototype_get, valid, if_float_multiply, index, start, length)
-                            else:
-                                set += set_only_min_or_max_function(
-                                    prototype_set, valid, if_float_multiply, index, start, length)
-
-                        if name.find('max') != -1:
-                            if previous_name.find('min') != -1:
-                                previous_name = previous_name.replace(
-                                    previous_name[len(previous_name) - 3:], 'max')
-                            if next_name.find('min') != -1:
-                                next_name = next_name.replace(
-                                    next_name[len(next_name) - 3:], 'max')
-
-                            if name == previous_name or name == next_name:
-                                set += set_max_function(
-                                    prototype_set, prototype_get, valid, if_float_multiply, index, start, length)
-                            else:
-                                set += set_only_min_or_max_function(
-                                    prototype_set, valid, if_float_multiply, index, start, length)
+                        set += set_min_max_function(prototype_set, valid, if_float_multiply, index, start, length)
                     else:
-                        set += set_function(prototype_set, valid,
-                                            if_float_multiply, index, start, length)
+                        set += set_function(prototype_set, valid, if_float_multiply, index, start, length)
 
                 # generate getters functions
                 if prototype_get != '':
@@ -252,32 +184,19 @@ def get_canbus_source(node, mode, messages):
                 control_bit_function = f"can_signal_read({index}, {control_bit_position}, 1)"
                 if node in signal['getters']:
                     if 'update' in signal:
-                        control_bit += update_function(name,
-                                                       control_bit_function)
+                        control_bit += update_function(name, control_bit_function)
 
                     elif 'control' in signal:
-                        control_bit += control_function(name,
-                                                        control_bit_function)
+                        control_bit += control_function(name, control_bit_function)
 
                     elif 'calibration' in signal:
-                        control_bit += calibration_function(
-                            name, control_bit_function)
+                        control_bit += calibration_function(name, control_bit_function)
 
     text = f'''\
 #include "canbus.h"
 #include "common.h"
-#include "can_signal.h"
-
-static int64_t convert_if_negative(uint64_t value, uint8_t bit_length)
-{{
-    if (value & (1 << (bit_length - 1)))
-    {{
-        value = -((~value + 1) & (~0ull >> (64 - bit_length)));
-    }}
-
-    return (int64_t)value;
-}}
-
+#include "can_signal.h"\
+{convert}
 {set}
 {get}
 {control_bit}\
@@ -347,11 +266,11 @@ bool canbus_is_{name}_updated(void);
     return text
 
 
-def control_header(comment, name):
+def control_header(name):
     text = \
         f"""
 /**
- * @brief This function is used to check if {comment} is enabled.
+ * @brief This function is used to check if {name} is enabled.
  * @return true if signal is enabled; otherwise false
  */
 bool canbus_is_{name}_enabled(void);
@@ -385,37 +304,29 @@ def get_canbus_header(node, mode, messages):
             comment = signal.get('comment')
 
             if message['setter'] == node or node in signal['getters']:
-                description = ''
+                description = ""
                 if 'range' in signal:
-                    description = 'in the range of [{0}, {1}]'.format(
-                        signal['range'][0], signal['range'][1])
+                    description = f"in the range of [{signal['range'][0]}, {signal['range'][1]}]"
                 else:
                     description = ', '.join(signal['values'][:-1])
                     description += ' or ' + signal['values'][-1]
 
-                if message['setter'] == node and node in signal['getters']:
+                if message['setter'] == node:
                     set += set_header(comment, description, name, type)
+                    if node not in signal['getters']:
+                        if mode == 'dev':
+                            get += get_test_double_header(comment, description, name, type)
+                if node in signal['getters']:
                     get += get_header(comment, description, name, type)
-
-                elif message['setter'] == node and node not in signal['getters']:
-                    set += set_header(comment, description, name, type)
-
-                    if mode == 'dev':
-                        get += get_test_double_header(comment,
-                                                      description,  name, type)
-
-                elif message['setter'] != node and node in signal['getters']:
-                    get += get_header(comment, description, name, type)
-
-                    if mode == 'dev':
-                        set += set_test_double_header(comment,
-                                                      description, name, type)
+                    if message['setter'] != node:
+                        if mode == 'dev':
+                            set += set_test_double_header(comment, description, name, type)
 
                 if node in signal['getters']:
                     if 'update' in signal:
                         control_bit += update_header(comment, name)
                     elif 'control' in signal:
-                        control_bit += control_header(comment, name)
+                        control_bit += control_header(name)
                     elif 'calibration' in signal:
                         control_bit += calibration_header(comment, name)
 
